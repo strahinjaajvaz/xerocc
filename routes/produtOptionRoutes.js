58048cca99b8e5db2108a1b4d70624e5a116fdde
Strahinja Ajvaz <strahinja.ajvaz@gmail.com>
2020-06-17 22:49:29 +1000

    added productOptionRoutes

---
+++ routes/produtOptionRoutes.js
@@ -0,0 +1,6 @@
+const uuid = require("uuid");
+let productOptions = require("../db/productOptions.json");
+
+module.exports = (app) => {
+  app.get("/products/:id/options", function (req, res) {});
+};
